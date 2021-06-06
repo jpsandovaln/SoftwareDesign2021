b20386140e9b51452809fef3247a6c21be34fe57
Paolo Sandoval <paolo.jalasoft@jalasoft.com>
2021-06-06 00:07:47 -0400

    Adde the machine learning dependencies on modular project

--- PROJECT_MODULARIZACION/machine_learning/src/main/java/module-info.java
+++ PROJECT_MODULARIZACION/machine_learning/src/main/java/module-info.java
@@ -1,3 +1,8 @@
 module machine_learning_project {
     exports com.jalasoft.machine_learning;
+    exports com.jalasoft.machine_learning.exception;
+    requires org.apache.commons.io;
+    requires ai.djl.api;
+    requires ai.djl.mxnet_engine;
+    requires ai.djl.mxnet_model_zoo;
 }
